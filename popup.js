9816ccfbc20df56b73145d1a833614aa64ff80af
umeraltaf095 <umergujjar095@gmail.com>
2025-05-01 18:52:36 +0500

    Added buttons for conversion into celsius or fahrenheit

--- popup.js
+++ popup.js
@@ -3,23 +3,56 @@ const locate = document.getElementById("selectLocation");
 const tem = document.getElementById("temp-value");
 const feelsLike = document.getElementById("feelsLike-value")
 const country = document.getElementById("selCountry");
+const humidity = document.getElementById("humidity-value");
+const windSpeed = document.getElementById("windSpeed-value");
+const clouds = document.getElementById("clouds-value");
+const minTemp = document.getElementById("minTemp-value");
+const maxTemp = document.getElementById("maxTemp-value");
+const clesiusBtn = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+let unit = 'metric' ;
 
-locate.addEventListener('change',()=>{
-    console.log('Current location is ', locate.value);
+clesiusBtn.addEventListener('click',()=>{
+  console.log("Button clicked");
+  unit = 'metric';
+  getWeather();
+
+  
+})
+
+fahrenheit.addEventListener('click', ()=>{
+  unit = 'imperial';
+  getWeather();
+})
+
+function getWeather(){
   const loca = locate.value;
-  const data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loca}&appid=2909ba420cd66f3878854604c3d0a67a&units=metric`)
+  const data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loca}&appid=2909ba420cd66f3878854604c3d0a67a&units=${unit}`)
 .then(response => response.json())
 .then(data=> {
     console.log(data)
     console.log(data.main.temp);
     tem.textContent = data.main.temp;
     feelsLike.textContent = data.main.feels_like;
+    humidity.textContent = data.main.humidity;
+    windSpeed.textContent = data.wind.speed;
+    clouds.textContent = data.clouds.all;
+    minTemp.textContent = data.main.temp_min;
+    maxTemp.textContent = data.main.temp_max;
+
 
     
 })
 .catch(error => console.error('Error:', error));
+
+}
+
+
+
+locate.addEventListener('change', getWeather);
     
-});
+  
+    
 
 fetch('./data/countries.json')
 .then(response => response.json())
